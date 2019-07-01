4365afce14c106be8c8ec6b720723c7bd71f8fb8
unknown <wucl@tpam.local>
2019-07-01 15:26:00 +0800

    d

--- Print_List_of_Even_Numbers.py
+++
@@ -1,8 +0,0 @@
-# INPUT NUMBER OF EVEN NUMBERS
-
-n=int(input('Amount: '))
-start=0
-
-for i in range(n):
-  print(start)
-  start+=2
--- Print_List_of_Odd_Numbers.py
+++
@@ -1,8 +0,0 @@
-# INPUT NUMBER OF ODD NUMBERS
-
-n=int(input('Amount: '))
-start=1
-
-for i in range(n):
-  print(start)
-  start+=2
